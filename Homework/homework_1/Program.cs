e2d13c9d64de1d118723018b97de5729d30925f7
Elena Pavliukevich <elenaivanovna0585@gmail.com>
2023-02-25 20:47:12 +0300

    Task6

--- Homework/homework_1/Program.cs
+++ Homework/homework_1/Program.cs
@@ -22,31 +22,48 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
-Console.WriteLine("Input number a:");
-int numbera = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input number a:");
+//int numbera = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input number b:");
-int numberb = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input number b:");
+//int numberb = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input number c:");
-int numberc = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if (numbera > numberb)
+//Console.WriteLine("Input number c:");
+//int numberc = Convert.ToInt32(Console.ReadLine());
+//int max = 0;
+//if (numbera > numberb)
+//{
+//if (numbera > numberc)
+//{
+  //Console.WriteLine(max = numbera);
+//}
+//else
+//{
+  //Console.WriteLine(max = numberc);
+//}
+//}
+//else if (numberb > numberc)
+//{
+ //Console.WriteLine(max = numberb); 
+//}
+//else
+//{
+  //Console.WriteLine(max = numberc);
+//}
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
 {
-if (numbera > numberc)
-{
-  Console.WriteLine(max = numbera);
+  Console.WriteLine("numberEven");
 }
 else
+  if(number % 2 != 0)
 {
-  Console.WriteLine(max = numberc);
-}
-}
-else if (numberb > numberc)
-{
- Console.WriteLine(max = numberb); 
-}
-else
-{
-  Console.WriteLine(max = numberc);
+  Console.WriteLine("numberOdd");
 }
