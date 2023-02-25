c9c492a7bd3047e2301bcb879a8550ed017a1109
Elena Pavliukevich <elenaivanovna0585@gmail.com>
2023-02-25 21:35:18 +0300

    Task8

--- Homework/homework_1/Program.cs
+++ Homework/homework_1/Program.cs
@@ -55,15 +55,30 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
-Console.WriteLine("Input number ");
-int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input number ");
+//int number = Convert.ToInt32(Console.ReadLine());
 
-if(number % 2 == 0)
+//if(number % 2 == 0)
+//{
+  //Console.WriteLine("numberEven");
+//}
+//else
+//  if(number % 2 != 0)
+//{
+  //Console.WriteLine("numberOdd");
+//}
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Input  N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("numberEven from 1 ,to n");
+int num1 = 1;
+while(num1 <= n)
 {
-  Console.WriteLine("numberEven");
-}
-else
-  if(number % 2 != 0)
-{
-  Console.WriteLine("numberOdd");
+  if(num1 % 2 == 0)
+  Console.Write(num1 + ",");
+  num1 ++;
 }
